89d0936559c21b2d2e81a3c90f328655d370e4e3
Jeremias <jerexxypunto@gmail.com>
2021-08-06 12:38:21 -0400

    configuro header.php

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -10,50 +11,50 @@
  */
 
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
 
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/bootsrap.css">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/tablet.css" media="screen and (min-width:900px)">
+	<link rel="stylesheet" href="css/descktop.css" media="screen and (min-width:992px)">
+	<link rel="stylesheet" href="fontawesome/css/all.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500&family=Roboto+Slab:wght@300;400;500;600&display=swap" rel="stylesheet">
+	<title>Buses Sancor</title>
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'buses_sancor' ); ?></a>
+	<?php wp_body_open(); ?>
+	<div id="page" class="site">
+		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'buses_sancor'); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$buses_sancor_description = get_bloginfo( 'description', 'display' );
-			if ( $buses_sancor_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $buses_sancor_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'buses_sancor' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+		<div class="pre-header"></div>
+		<!-- Header -->
+		<header class="header">
+			<div class="container">
+				<div class="row navigator">
+					<div class="col-9 col-md-4 navigator_logo"><img class="img-fluid" src="img/sancor-logo.png" alt="logo"></div>
+					<div class="col-3 col-md-8 navigator_menu--button">
+						<nav> <span class="fas fa-bars"></span></nav>
+					</div>
+					<div class="col-12 col-md-6 navigator_menu" id="lista-menu">
+						<ul>
+							<li> <a href="#">Inicio</a></li>
+							<li> <a href="#">Flota</a></li>
+							<li> <a href="#">Nosotros</a></li>
+							<li> <a href="#">Preguntas Frecuentes</a></li>
+							<li> <a href="#">Contacto</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</header>
+		<!-- Header - END -->

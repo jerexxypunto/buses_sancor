7285fea38f7659506f7602cda09021991f99ccd0
Jeremias <jerexxypunto@gmail.com>
2021-08-06 12:39:37 -0400

    footer.php configurado

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -11,24 +12,34 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'buses_sancor' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'buses_sancor' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'buses_sancor' ), 'buses_sancor', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
+<!-- Footer -->
+<footer class="footer">
+	<h3>¡Contactanos! </h3>
+	<div class="footer__social">
+		<div class="footer__social__item"><i class="fab fa-facebook-f"></i>
+			<p>@buses.sancor</p>
+		</div>
+		<div class="footer__social__item"><i class="fab fa-whatsapp"></i>
+			<p>+56 999 999 99</p>
+		</div>
+		<div class="footer__social__item"><i class="far fa-envelope"></i>
+			<p>mail@mail.com</p>
+		</div>
+	</div>
+	<div class="footer__horarios">
+		<h4>Horarios de atención telefonica <i class="fas fa-clock"></i></h4>
+		<p>Lunes a Domingo 09:00 a 17:00</p>
+	</div>
+	<p>Buses Sancor &copy; Todos los derechos reservados</p>
+	<p>Desarrollado por <a href="www.gscdesigns.net">GSC Diseños</a></p>
+</footer>
+<!-- Footer - End -->
+<script src="fontawesome/js/all.js"></script>
+<script src="js/app.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 <?php wp_footer(); ?>
 
 </body>
+
 </html>

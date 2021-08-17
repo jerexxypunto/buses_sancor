1e184fdcd7c4c49d76da8ac4ddd47595a374270d
Jeremias <jerexxypunto@gmail.com>
2021-08-17 14:38:44 -0400

    quito the_title de page-contacto

--- page-contacto.php
+++ page-contacto.php
@@ -19,13 +19,11 @@ get_header();
 	<main id="page-contacto" class="page-contacto"> <!-- main -->
         <figure class="banner-flota">
            <img src="<?php echo get_template_directory_uri()?>/img/banner-contacto.jpg" alt="banner contacto">
-            <figcaption>
-                <h1><?php the_title() ?></h1>
-            </figcaption>
         </figure>
         <div class="container-fluid">
             <div class="row">
                 <form class="col-12 col-md-7" id="formulario" action="../enviar" method="post">
+                <h3>¿Con quién trabajaremos?</h3>
                     <div class="row">
                         <label class="col-10 pb-3">
                             Nombre de pila
@@ -33,15 +31,18 @@ get_header();
                         </label>
                         <span id="res-nombre" class="col-2"></span>
                         <label class="col-10 pb-3">
-                            Télefono
+                            Teléfono
                             <input class="form-control" id="telefono" type="text" placeholder="+56 999 999 999" name="telefono">
                         </label>
                         <span id="res-telefono col-2"></span>
                         <label class="col-10 pb-3">
-                            Correo electronico
+                            Correo electrónico
                             <input class="form-control" id="mail" type="email" placeholder="mail@mailito.com" name="correo">
                         </label>
                         <span id="res-mail" class="col-2"></span>
+                        <div class="col-12">
+                            <h3>Cuéntanos... ¿Cómo podemos ayudarte?</h3>
+                        </div>
                         <label class="col-10 pb-3">
                             Mensaje
                             <textarea class="form-control" placeholder="Ingrese su mensaje" name="mensaje" id="msg" cols="30" rows="10"></textarea>
@@ -53,7 +54,7 @@ get_header();
                             <div class="alert-container">
                                 <div class="alert alert-warning">
                                     <p> * El nombre de pila hace referencia sólo al primer nombre y al primer apellido. <p/> 
-                                    <p> * El teléfono se escribe sin simbolos extraños [-, +, $, etc.]. </p>
+                                    <p> * El teléfono se escribe sin símbolos extraños [-, +, $, etc.]. </p>
                                 </div>
                                 <div class="alert alert-danger">
                                     Por favor NO deje ninguna casilla en blanco, si las casillas no se llenan correctamente, no podrá enviarse el formulario.
@@ -63,6 +64,7 @@ get_header();
                     </div>
                 </form>
                 <div class="col-12 col-md-5 map">
+                    <h3>¿Cómo encontrarnos?</h3>
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3201.9429147143173!2d-72.1176837!3d-36.627760699999996!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9669283e194d3a1b%3A0x8d8c06056bd22fa2!2sLas+Canoas+472%2C+Chill%C3%A1n%2C+Biob%C3%ADo%2C+Chile!5e0!3m2!1ses-419!2s!4v1424548300234"
                     >

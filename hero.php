acb719f4ffce9d2cfce72281503ab6185818150b
Jeremias <jerexxypunto@gmail.com>
2021-09-01 19:53:31 -0400

    img-resónsive slider

--- hero.php
+++ hero.php
@@ -2,20 +2,52 @@
     <div id="carouselExampleControls" class="carousel slide"  data-ride="carousel">
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img src="<?php echo get_template_directory_uri(); ?>/img/Hero/hero-1.png" class="d-block w-100" alt="...">
+          <picture>
+              <source
+                media="screen and (max-width:600px)" 
+                srcset="<?php echo get_template_directory_uri(); ?>/img/Hero/hero-1-sm.png">  
+              <source
+                media="screen and (max-width:900px)" 
+                srcset="<?php echo get_template_directory_uri(); ?>/img/Hero/hero-1-md.png">  
+              <img 
+                src="<?php echo get_template_directory_uri(); ?>/img/Hero/hero-1.png" 
+                class="d-block w-100" 
+                alt="...">
+          </picture>
           <div class="contenedor-boton-carrousel">
             <a class="boton-enlace" href="./#hacemos"><span class="triangle triangle-left"></span>Leer más<span class="triangle triangle-rigth"></span></a>
           </div>
         </div>
         <div class="carousel-item">
-          <img src="<?php echo get_template_directory_uri(); ?>/img/Hero/hero-2.png"" class=" d-block w-100" alt="...">
+          <picture>
+            <source
+                media="screen and (max-width:600px)" 
+                srcset="<?php echo get_template_directory_uri(); ?>/img/Hero/hero-2-sm.png">  
+              <source
+                media="screen and (max-width:900px)" 
+                srcset="<?php echo get_template_directory_uri(); ?>/img/Hero/hero-2-md.png">  
+              <img 
+                src="<?php echo get_template_directory_uri(); ?>/img/Hero/hero-2.png"
+                class="d-block w-100"
+                alt="...">
+          </picture>
           <div class="contenedor-boton-carrousel">
             <a class="boton-enlace" href="./contacto"><span class="triangle triangle-left"></span>Leer más<span class="triangle triangle-rigth"></span></a>
           </div>
         </div>
         <div class="carousel-item">
-          <img src="<?php echo get_template_directory_uri(); ?>/img/Hero/hero-3.jpg" class="d-block w-100" alt="...">
-          <div class="contenedor-boton-carrousel">
+          <picture>
+              <source
+                media="screen and (max-width:600px)" 
+                srcset="<?php echo get_template_directory_uri(); ?>/img/Hero/hero-3-sm.jpg">  
+              <source
+                media="screen and (max-width:900px)" 
+                srcset="<?php echo get_template_directory_uri(); ?>/img/Hero/hero-3-md.jpg">  
+              <img 
+                src="<?php echo get_template_directory_uri(); ?>/img/Hero/hero-3.jpg"
+                class="d-block w-100"
+                alt="...">  
+          </picture>         <div class="contenedor-boton-carrousel">
             <a class="boton-enlace" href="./flota"><span class="triangle triangle-left"></span>Leer más<span class="triangle triangle-rigth"></span></a>
           </div>
         </div>

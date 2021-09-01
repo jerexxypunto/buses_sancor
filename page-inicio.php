656101b99877876064798eb2472b4b140e324e0b
Jeremias <jerexxypunto@gmail.com>
2021-09-01 16:50:48 -0400

    modulariso el hero

--- page-inicio.php
+++ page-inicio.php
@@ -24,38 +24,8 @@ get_header();
 }(document, 'script', 'facebook-jssdk'));</script>
 <main>
   <!-- Hero -->
-  <section id="hero">
-    <div id="carouselExampleControls" class="carousel slide"  data-ride="carousel">
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="<?php echo get_template_directory_uri(); ?>/img/Hero/hero-1.png" class="d-block w-100" alt="...">
-          <div class="contenedor-boton-carrousel">
-            <a class="boton-enlace" href="./#hacemos"><span class="triangle triangle-left"></span>Leer más<span class="triangle triangle-rigth"></span></a>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <img src="<?php echo get_template_directory_uri(); ?>/img/Hero/hero-2.png"" class=" d-block w-100" alt="...">
-          <div class="contenedor-boton-carrousel">
-            <a class="boton-enlace" href="./contacto"><span class="triangle triangle-left"></span>Leer más<span class="triangle triangle-rigth"></span></a>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <img src="<?php echo get_template_directory_uri(); ?>/img/Hero/hero-3.jpg" class="d-block w-100" alt="...">
-          <div class="contenedor-boton-carrousel">
-            <a class="boton-enlace" href="./flota"><span class="triangle triangle-left"></span>Leer más<span class="triangle triangle-rigth"></span></a>
-          </div>
-        </div>
-      </div>
-      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div>
-  </section>
+  <!-- inclyo modulo de hero -->
+  <?php include 'hero.php' ?>
   <!-- Hero - end -->
   <!-- ¿Qué hacemos? -->
   <section id="hacemos">

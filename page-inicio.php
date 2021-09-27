8bf8b3d139ccecd025b5675f46cbb150b88e0c8c
Jeremias <jerexxypunto@gmail.com>
2021-09-27 17:01:50 -0300

    alineo elementos en page-inicio

--- page-inicio.php
+++ page-inicio.php
@@ -46,91 +46,95 @@ get_header();
       </figure>
     </div>
     <h1 class="text-center tituloDeSeccion"><span class="black-text">¿</span> Por qué elegir Buses Sancor <span class="black-text">?</span></h1>
-      <h3 class="text-center">Nos tomamos enserio tu seguridad y comodidad</h3>
-      <div class="inicio-box-container">
-        <div class="inicio-box">
-          <div class="row">
-            <div class="col-12 col-md-6 inicio-box__info">
-              <h4>Nuestra flota</h4>
-              <p>Contamos con una flota moderna y acorde a las necesidades de nuestros clientes, permitiendo la realización del
-                servicio
-                solicitado de una manera confortable y eficaz. Para ello realizamos mantenciones periódicas a todos nuestros
-                vehículos
-                que nos permiten contar con un amplio respaldo de nuestros usuarios.</p>
+      <div class="container">
+        <h3 class="text-center">Nos tomamos enserio tu seguridad y comodidad</h3>
+        <div class="inicio-box-container">
+          <div class="inicio-box">
+            <div class="row">
+              <div class="col-12 col-md-6 inicio-box__info">
+                <h4>Nuestra flota</h4>
+                <p>Contamos con una flota moderna y acorde a las necesidades de nuestros clientes, permitiendo la realización del
+                  servicio
+                  solicitado de una manera confortable y eficaz. Para ello realizamos mantenciones periódicas a todos nuestros
+                  vehículos
+                  que nos permiten contar con un amplio respaldo de nuestros usuarios.</p>
+              </div>
+              <div class="col-12 col-md-6 p-0">
+                <figure>
+                  <img src="<?php echo get_template_directory_uri(); ?>/img/buses.png" alt="buses">
+                </figure>
+              </div>
             </div>
-            <div class="col-12 col-md-6 p-0">
-              <figure>
-                <img src="<?php echo get_template_directory_uri(); ?>/img/buses.png" alt="buses">
-              </figure>
+          </div>
+          <div class="inicio-box">
+            <div class="row">
+              <div class="col-12 col-md-6 inicio-box__info">
+                <h4>Mantenimiento exhaustivo</h4>
+                <p>Nuestra flota se encuentra periódicamente sometida a mantenimiento en sus diversos ámbitos como carrocería, motor,
+                  luces, neumáticos, etc. para ello nuestra empresa cuenta con un taller exclusivo en el cual se realizan estas
+                  diversas
+                  operaciones por un personal capacitado para ello.
+                </p>
+              </div>
+              <div class="col-12 col-md-6 p-0">
+                <figure>
+                  <img src="<?php echo get_template_directory_uri(); ?>/img/tools.png" alt="Herramientas">
+                </figure>
+              </div>
+            </div>
+          </div>
+          <div class="inicio-box">
+            <div class="row">
+              <div class="col-12 col-md-6 inicio-box__info">
+                <h4>Servicio de calidad</h4>
+                <p>Contamos con un personal capacitado y grato que nos permite ofrecer un servicio cálido en la fecha y hora que
+                  lo
+                  solicites, como también todas tus consultas o dudas son respondidas en un tiempo acorde a las necesidades de hoy
+                  en día,
+                  por último destacar que nuestros vehículos cuentan con todos los elementos necesarios para brindar la mayor
+                  seguridad en
+                  nuestros servicios.
+                </p>
+              </div>
+              <div class="col-12 col-md-6 p-0">
+                <figure>
+                  <img src="<?php echo get_template_directory_uri(); ?>/img/happy.png" alt="happy">
+                </figure>
+              </div>
             </div>
           </div>
         </div>
-        <div class="inicio-box">
-          <div class="row">
-            <div class="col-12 col-md-6 inicio-box__info">
-              <h4>Mantenimiento exhaustivo</h4>
-              <p>Nuestra flota se encuentra periódicamente sometida a mantenimiento en sus diversos ámbitos como carrocería, motor,
-                luces, neumáticos, etc. para ello nuestra empresa cuenta con un taller exclusivo en el cual se realizan estas
-                diversas
-                operaciones por un personal capacitado para ello.
-              </p>
-            </div>
-            <div class="col-12 col-md-6 p-0">
-              <figure>
-                <img src="<?php echo get_template_directory_uri(); ?>/img/tools.png" alt="Herramientas">
-              </figure>
-            </div>
-          </div>
-        </div>
-        <div class="inicio-box">
-          <div class="row">
-            <div class="col-12 col-md-6 inicio-box__info">
-              <h4>Servicio de calidad</h4>
-              <p>Contamos con un personal capacitado y grato que nos permite ofrecer un servicio cálido en la fecha y hora que
-                lo
-                solicites, como también todas tus consultas o dudas son respondidas en un tiempo acorde a las necesidades de hoy
-                en día,
-                por último destacar que nuestros vehículos cuentan con todos los elementos necesarios para brindar la mayor
-                seguridad en
-                nuestros servicios.
-              </p>
-            </div>
-            <div class="col-12 col-md-6 p-0">
-              <figure>
-                <img src="<?php echo get_template_directory_uri(); ?>/img/happy.png" alt="happy">
-              </figure>
+        <h3 class="text-center">Nos preocupa que aproveches tus recursos</h3>
+        <div class="inicio-box-container">
+          <div class="inicio-box">
+            <div class="row">
+              <div class="col-12 col-md-6 p-0">
+                <figure>
+                  <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/img/Hablar.png" alt="dos personas hablando">
+                </figure>
+              </div>
+              <div class="col-12 col-md-6 inicio-box__info">
+                <h4>La comunicación es importante.</h4>
+                <p>Te ofrecemos diversas maneras de comunicarte con nuestra empresa de manera que todas tus consultas sobre el viaje a
+                  realizar puedan ser resueltas a la brevedad como por ejemplo el valor del viaje, disponibilidad, etc. además de
+                  poder
+                  reservar el vehículo para tu viaje en la fecha que más acomode a tus necesidades.</p>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-      <h3 class="text-center">Nos preocupa que aproveches tus recursos</h3>
-      <div class="inicio-box-container">
-        <div class="inicio-box">
-          <div class="row">
-            <div class="col-12 col-md-6 p-0">
-              <figure>
-                <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/img/Hablar.png" alt="dos personas hablando">
-              </figure>
-            </div>
-            <div class="col-12 col-md-6">
-              <h4>La comunicación es importante.</h4>
-              <p>Te ofrecemos diversas maneras de comunicarte con nuestra empresa de manera que todas tus consultas sobre el viaje a
-                realizar puedan ser resueltas a la brevedad como por ejemplo el valor del viaje, disponibilidad, etc. además de
-                poder
-                reservar el vehículo para tu viaje en la fecha que más acomode a tus necesidades.</p>
-            </div>
-          </div>
-        </div>
-        <div class="inicio-box">
-          <div class="row">
-            <div class="col-12 col-md-6 p-0">
-              <figure>
-                <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/img/Ahorro.png" alt="Ahorro">
-              </figure>
-            </div>
-            <div class="col-12 col-md-6">
-              <h4>¡No gastes demás!</h4>
-              <p>Nuestros clientes pueden acceder a algunos descuentos de acorde al presupuesto que deseare plantear.</p>
+          <div class="inicio-box">
+            <div class="row">
+              <div class="col-12 col-md-6 p-0">
+                <figure>
+                  <img class="img-fluid" src="<?php echo get_template_directory_uri(); ?>/img/Ahorro.png" alt="Ahorro">
+                </figure>
+              </div>
+              <div class="col-12 col-md-6 inicio-box__info">
+                <div>
+                  <h4>¡No gastes demás!</h4>
+                  <p>Nuestros clientes pueden acceder a algunos descuentos de acorde al presupuesto que deseare plantear.</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
